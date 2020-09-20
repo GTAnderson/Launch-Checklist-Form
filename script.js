e8413bf57df0b8f9fc6e9f78ab74540a10664a6c
Garrett Anderson <garrett@Garretts-MacBook-Pro.local>
2020-09-20 14:26:43 -0500

    Cleaned up unnecessary code

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.addEventListener("load", function() {
       let copilotnameInput = document.querySelector("input[name=copilotName]");
       let fuellevelInput = document.querySelector("input[name=fuelLevel]");
       let cargomassInput = document.querySelector("input[name=cargoMass]");
+      
       if (pilotnameInput.value === "" || copilotnameInput.value === "" || fuellevelInput.value === "" || cargomassInput.value === "") {
          alert("All fields are required!");
          event.preventDefault();
@@ -18,47 +19,26 @@ window.addEventListener("load", function() {
          alert("Fuel Level and / or Cargo Mass is not a number!");
          event.preventDefault();
       };
-      function pilotstatusUpdate() {
-         document.getElementById("pilotStatus").innerHTML = `${pilotnameInput.value}`;
-         console.log(`${pilotnameInput.value}`);
-      }
-      function copilotstatusUpdate() {
-         document.getElementById("copilotStatus").innerHTML = `${copilotnameInput.value}`;
-         console.log(`${copilotnameInput.value}`);
-      }
-      let pilot_status = document.getElementById("pilotStatus");
-      let copilot_status = document.getElementById("copilotStatus");
-      pilot_status.addEventListener("submit", pilotstatusUpdate);
-      copilot_status.addEventListener("submit", copilotstatusUpdate);
-
-      function fuellevelUpdate() {
+      
+      // Using template literals, update the li elements pilotStatus and copilotStatus to include the pilot's name and the co-pilot's name.
+      document.getElementById("pilotStatus").innerHTML = `${pilotnameInput.value}`;
+      document.getElementById("copilotStatus").innerHTML = `${copilotnameInput.value}`;
+      
+      if (fuellevelInput.value < 10000 === true) {
          $(this).css("faultyItems", visible);
          document.getElementById("fuelStatus").innerHTML = `${fuellevelInput.value} is not enough fuel for the journey.`
          document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch.`
          document.getElementById("launchStatus").color = red;   
-      }
-      function cargomassUpdate() {
+      } else if (cargomassInput.value > 10000 === true) {
          $(this).css("faultyItems", visible);
          document.getElementById("cargoStatus").innerHTML = `${cargomassInput.value} is too much mass for the shuttle to take off.`
          document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch.`
          document.getElementById("launchStatus").color = red;
-      }
-      function launchstatusUpdate() {
+      } else {
          document.getElementById("launchStatus").innerHTML = `Shuttle is ready for launch.`
          document.getElementById("launchStatus").color = green;
-      }
-      
-      if (fuellevelInput.value < 10000 === true) {
-         let fuel_level = document.getElementById("fuelStatus");
-         fuel_level.addEventListener("submit", fuellevelUpdate);
-      } else if (cargomassInput.value > 10000 === true) {
-         let cargo_mass = document.getElementById("cargoStatus");
-         cargo_mass.addEventListener("submit", cargomassUpdate);
-      } else {
-         let launch_status = document.getElementById("launchStatus");
-         launch_status.addEventListener("submit", launchstatusUpdate);
       };
-          
+
    });
 });
 
