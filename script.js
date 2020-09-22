0c204536ba9f3787d59960fc9f55dcff1b47e747
Garrett Anderson <garrett@Garretts-MBP.lan1>
2020-09-21 21:09:26 -0500

    fixed event.preventDefault position issue

--- script.js
+++ script.js
@@ -2,43 +2,36 @@
 window.addEventListener("load", function() {
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
+      event.preventDefault();
       let pilotnameInput = document.querySelector("input[name=pilotName]");
       let copilotnameInput = document.querySelector("input[name=copilotName]");
       let fuellevelInput = document.querySelector("input[name=fuelLevel]");
       let cargomassInput = document.querySelector("input[name=cargoMass]");
-
       if (pilotnameInput.value === "" || copilotnameInput.value === "" || fuellevelInput.value === "" || cargomassInput.value === "") {
          alert("All fields are required!");
-         event.preventDefault();
       };
       if (isNaN (pilotnameInput.value) === false || isNaN (copilotnameInput.value) === false) {
          alert("Pilot Name and / or Co-Pilot Name is not a string!");
-         event.preventDefault();
       };
       if (isNaN (fuellevelInput.value) === true || isNaN (cargomassInput.value) === true) {
          alert("Fuel Level and / or Cargo Mass is not a number!");
-         event.preventDefault();
       };
-      
-      // Using template literals, update the li elements pilotStatus and copilotStatus to include the pilot's name and the co-pilot's name.
       document.getElementById("pilotStatus").innerHTML = `${pilotnameInput.value}`;
-      document.getElementById("copilotStatus").innerHTML = `${copilotnameInput.value}`;
-      
-      if (fuellevelInput.value < 10000 === true) {
-         document.getElementById("faultyItems").style.visibility = visible;
-         document.getElementById("fuelStatus").innerHTML = `${fuellevelInput.value} is not enough fuel for the journey.`
+      document.getElementById("copilotStatus").innerHTML = `${copilotnameInput.value}`;      
+      if (fuellevelInput.value < 10000) {
+         document.getElementById("faultyItems").style.visibility = "visible";
+         document.getElementById("fuelStatus").innerHTML = `${fuellevelInput.value} L is not enough fuel for the journey.`
          document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch.`
-         document.getElementById("launchStatus").style.color = red;   
-      } else if (cargomassInput.value > 10000 === true) {
-         document.getElementById("faultyItems").style.visibility = visible;
-         document.getElementById("cargoStatus").innerHTML = `${cargomassInput.value} is too much mass for the shuttle to take off.`
+         document.getElementById("launchStatus").style.color = "red";   
+      } else if (cargomassInput.value > 10000) {
+         document.getElementById("faultyItems").style.visibility = "visible";
+         document.getElementById("cargoStatus").innerHTML = `${cargomassInput.value} kg is too much mass for the shuttle to take off.`
          document.getElementById("launchStatus").innerHTML = `Shuttle not ready for launch.`
-         document.getElementById("launchStatus").style.color = red;
+         document.getElementById("launchStatus").style.color = "red";
       } else {
          document.getElementById("launchStatus").innerHTML = `Shuttle is ready for launch.`
-         document.getElementById("launchStatus").style.color = green;
+         document.getElementById("launchStatus").style.color = "green";
       };
-
    });
 });
 
